e9726dcb605c1147599e50c46d2447386bd98c39
LucasMeloSena <lucasmelosena@gmail.com>
2023-12-11 21:53:41 -0300

    feat: 404 page

--- client/src/pages/Page404/index.tsx
+++ client/src/pages/Page404/index.tsx
@@ -2,17 +2,30 @@ import {
   MainContent,
   WarmContainer,
   TitleWarm,
+  SubtitleWarm,
   ImgBrokenLight,
+  TextContainer,
+  DescriptionWarm,
+  BtnGoHome,
+  ArrowIcon
 } from "../../assets/styles/pages/Page404/Page404.style";
 import BrokenLightImage from "../../assets/img/404/broken-light.png";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function Page404() {
   return (
     <>
       <MainContent>
         <WarmContainer>
-            <ImgBrokenLight src={BrokenLightImage} alt="broken-light" />
+          <ImgBrokenLight src={BrokenLightImage} alt="broken-light" />
+          <TextContainer>
             <TitleWarm>404</TitleWarm>
+            <SubtitleWarm>LOOKS LIKE YOU ARE LOST!</SubtitleWarm>
+            <DescriptionWarm>
+              The page you are looking for are not available!
+            </DescriptionWarm>
+            <BtnGoHome>GO TO HOME <ArrowIcon icon={faArrowRight}/></BtnGoHome>
+          </TextContainer>
         </WarmContainer>
       </MainContent>
     </>
