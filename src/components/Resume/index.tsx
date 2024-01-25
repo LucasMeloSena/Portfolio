f50c95bedd9047fcf62c2bc2457a3c18bda48ec0
Lucas Sena <93053816+LucasMeloSena@users.noreply.github.com>
2024-01-25 01:04:06 +0000

    initial `Resume` component

---
+++ src/components/Resume/index.tsx
@@ -0,0 +1,14 @@
+import { ContainerResume } from "src/styles/components/Resume/Resume.style";
+import { Title } from "@/PortfolioUI";
+
+function Resume() {
+  return (
+    <>
+      <ContainerResume>
+        <Title text={"Resume"} />
+      </ContainerResume>
+    </>
+  );
+}
+
+export default Resume;
---
+++ src/styles/components/Resume/Resume.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ContainerResume = styled.div`
+  width: 100%;
+  height: 600px;
+  background: rgba(93, 97, 103, 0.36);
+  margin-top: 80px;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding-top: 20px;
+  margin-bottom: 500px;
+`;
