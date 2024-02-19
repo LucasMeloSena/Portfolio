92b45616a51405f91224eeabe4f722af9f1acfe9
LucasMeloSena <lucasmelosena@gmail.com>
2024-02-19 20:34:44 -0300

    fix bugs

--- src/pages/api/v1/access/index.ts
+++ src/pages/api/v1/access/index.ts
@@ -2,48 +2,31 @@ import { NextApiRequest, NextApiResponse } from "next";
 import database from "src/infra/database";
 
 async function acess(req: NextApiRequest, res: NextApiResponse) {
-  //@ts-ignore
-  req;
-
   try {
-    let result: any;
+    const ipAdress = req.connection.remoteAddress;
     let usuarioIgual;
-    await fetch("http://ip-api.com/json/", {
-      method: "GET",
-    })
-      .then(async (response) => {
-        result = await response.json();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    const date = new Date().toDateString();
 
-    if (result) {
-      const date = new Date().toDateString();
-
-      const cadastros = await database.query("SELECT * FROM ACESSO");
-      if (cadastros.rows.length) {
-        usuarioIgual = cadastros.rows.filter(
-          (item: ResultSearchDatabase) => item.ip == result.query,
-        );
-        let dataCadastros = cadastros.rows;
-        let warmAcess = usuarioIgual.filter(
-          (item: ResultSearchDatabase, index: number) =>
-            item.dt_acesso == dataCadastros[index].dt_acesso,
-        );
-        if (warmAcess.length > 0) {
-          throw new Error("Este usuário já acessou o site hoje!");
-        }
+    const cadastros = await database.query("SELECT * FROM ACESSO");
+    if (cadastros.rows.length) {
+      usuarioIgual = cadastros.rows.filter(
+        (item: ResultSearchDatabase) => item.ip == ipAdress,
+      );
+      let dataCadastros = cadastros.rows;
+      let warmAcess = usuarioIgual.filter(
+        (item: ResultSearchDatabase, index: number) =>
+          item.dt_acesso == dataCadastros[index].dt_acesso,
+      );
+      if (warmAcess.length > 0) {
+        throw new Error("Este usuário já acessou o site hoje!");
       }
-
-      await database.query({
-        text: "INSERT INTO acesso (IP, DT_ACESSO) VALUES ($1, $2)",
-        values: [result.query, date],
-      });
-      res.status(201).send({ message: "Acesso adicionado com sucesso!" });
-    } else {
-      throw new Error("Ocorreu um erro ao tentar adicionar um novo acesso!");
     }
+
+    await database.query({
+      text: "INSERT INTO acesso (IP, DT_ACESSO) VALUES ($1, $2)",
+      values: [ipAdress, date],
+    });
+    res.status(201).send({ message: "Acesso adicionado com sucesso!" });
   } catch (error) {
     res.status(500).send({ message: (error as Error).message });
   }
