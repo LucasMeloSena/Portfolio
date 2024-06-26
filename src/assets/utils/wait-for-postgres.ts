d2b788ed8c3aa3fc06bccef1b7834897e9895761
Lucas Sena <lucasmelosena@gmail.com>
2024-06-26 16:06:12 -0300

    fix wait process bug

--- src/assets/utils/wait-for-postgres.ts
+++ src/assets/utils/wait-for-postgres.ts
@@ -1,20 +1,21 @@
-import { exec } from 'node:child_process'
+import { exec } from "node:child_process";
 
 function checkPostgres() {
-  const command = "docker exec postgres-dev-portfolio pg_isready -h localhost -p 5432 -U docker -d portfolio"
+  const command =
+    "docker exec postgres-dev-portfolio pg_isready -h localhost -p 5432 -U docker -d portfolio";
 
-  exec(
-    command,
-    (error, stdout) => {
-      if (stdout.search("accepting connections") === -1) {
-        checkPostgres();
-        console.log("🔴 Aguardando Postgres Aceitar Conexões...");
-        return;
-      }
+  exec(command, (error, stdout) => {
+    if (error != null) {
+      console.log("Erro de identificação do docker container.");
+      return;
+    } else if (stdout.search("accepting connections") === -1) {
+      checkPostgres();
+      console.log("🔴 Aguardando Postgres Aceitar Conexões...");
+      return;
+    }
 
-      console.log("🟢 Postgres Aceitando Conexões...");
-    },
-  );
+    console.log("🟢 Postgres Aceitando Conexões...");
+  });
 }
 
 checkPostgres();
